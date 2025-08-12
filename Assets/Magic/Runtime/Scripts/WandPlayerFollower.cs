27fb1ad030c724e5d77acd752b3e4f6a1d8136c6
DeltaNeverUsed <sinus06@hotmail.com>
2025-08-12 13:18:00 +0200

    feat: make wand follow player when the local player gets too far away

---
+++ Assets/Magic/Runtime/Scripts/WandPlayerFollower.cs
@@ -0,0 +1,38 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+
+// ReSharper disable once CheckNamespace
+namespace BefuddledLabs.Magic {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class WandPlayerFollower : UdonSharpBehaviour {
+
+        public float maxDistance = 1.1f;
+
+        private Rigidbody _rb;
+        private VRCPlayerApi _player;
+
+        private bool _movingTowardsPlayer;
+        private void Start() {
+            _rb = GetComponent<Rigidbody>();
+            _player = Networking.LocalPlayer;
+            if (Networking.IsOwner(gameObject))
+                SendCustomEventDelayedFrames(nameof(Loop), 1);
+        }
+
+        public void Loop() {
+            Vector3 playerPos = _player.GetBonePosition(HumanBodyBones.Chest);
+            float height = _player.GetAvatarEyeHeightAsMeters();
+
+            if (!_movingTowardsPlayer)
+                _movingTowardsPlayer = Vector3.Distance(playerPos, transform.position) > height * maxDistance;
+            else {
+                _rb.velocity = Vector3.Lerp(_rb.velocity, playerPos - transform.position, 0.5f * Time.deltaTime);
+                _movingTowardsPlayer = Vector3.Distance(playerPos, transform.position) > height;
+            }
+            
+            SendCustomEventDelayedFrames(nameof(Loop), 1);
+        }
+    }
+}
---
+++ Assets/Magic/Runtime/Scripts/WandPlayerScaler.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+// ReSharper disable once CheckNamespace
+namespace BefuddledLabs.Magic {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class WandPlayerScaler : UdonSharpBehaviour {
+        private Vector3 _originalScale;
+
+        private void Start() {
+            _originalScale = transform.localScale / 1.2f;
+        }
+
+        public override void OnAvatarEyeHeightChanged(VRCPlayerApi player, float prevEyeHeightAsMeters) {
+            if (!player.IsOwner(gameObject))
+                return;
+
+            transform.localScale = _originalScale * player.GetAvatarEyeHeightAsMeters();
+        }
+    }
+}
